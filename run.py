e8a82cf7a6ce3de835f2a710fe598d9b1497e29a
Katia Dias <0j0zirbo@students.codeinstitute.net>
2024-12-28 00:40:26 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,26 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#Initialization
+"""
+- user vs computer
+- create two boards one for each player
+- place ships for both players randomly
+
+"""
+
+#Game loop
+""" 
+-show current player's board
+- ask for coordinates
+- check if coordinates are valid
+- check if hit or miss
+   - hit add one value to that player
+   - miss no add nothing 
+- update board with each move
+-switch to next player
+"""
+
+#End of game
+"""
+check if all of player's ships have been destroyed
+declare winner
+
+"""
